ba024c2983703ed66891e4dc9907ccaf5044c1f1
kd <erm.melnik@gmail.com>
2025-02-18 16:50:39 +0100

    Rearranging inside class Node

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -6,15 +6,14 @@ public class MyLinkedList implements MyList {
 
     @Override
     public void add(Person element) {
-        node = new Node(null, size, element , null);
+        node = new Node(null, element, null);
         if (head == null){
             head = node;
-            tail = node;
         } else {
             tail.setNext(node);
             node.setPrev(tail);
-            tail = node;
         }
+        tail = node;
         size++;
     }
 
@@ -26,15 +25,26 @@ public class MyLinkedList implements MyList {
     @Override
     public Person get(int index) {
         if (index>=0 && index<=size){
-            Node current = head;
-            while (current != null) {
-                if (current.getIndex() == index) {
-                    return current.getValue();
-                }
-                current = current.getNext();
-            }
+            return getByIndex(index);
         }
         return null;
+    }
+
+    private Person getByIndex(int index){
+        int counter = 0;
+        if(index<0 || index>=size){
+            return null;
+        }
+        Node current = head;
+        while (current != null && counter<index) {
+            if (current.getIndex() == index) {
+                counter++;
+                return current.getValue();
+            }
+            counter++;
+            current = current.getNext();
+        }
+        return current.getValue();
     }
 
     @Override
@@ -46,4 +56,53 @@ public class MyLinkedList implements MyList {
             node = node.getNext();
         }
     }
+
+    @Override
+    public String toString(){
+        String result = "";
+        Node currentNode = head;
+        while (currentNode!=null){
+            result+= currentNode.getValue() + System.lineSeparator();
+            currentNode= currentNode.getNext();
+        }
+        return result;
+    }
+
+    public static class Node {
+        private Node prev;
+        private int index;
+        private Person value;
+        private Node next;
+
+        public Node(Node prev, Person value, Node next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+        }
+        public int getIndex() {
+            return index;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Person getValue() {
+            return value;
+        }
+        public void setValue(Person value) {
+            this.value = value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
 }
